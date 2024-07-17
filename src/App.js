3b46ef9c8de4607f9838d39dceacf06ccf6f33d9
Umair Tauni <engrumayr@gmail.com>
2024-07-17 12:56:33 +0500

    Header Footer Note, Components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+    </div>
+  );
 }
 
 export default App;
